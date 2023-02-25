fec594c9fc0c38684e6b31a10f4869b2063e37e4
oier <oieruni22@gmail.com>
2023-02-25 14:56:47 +0100

    [REFACTOR] Using inheritance to fix functions depending on the search problem

--- funciones.py
+++ funciones.py
@@ -1,45 +1,74 @@
-def pushSuccessors(successors, structure):
-    for s in successors:
-        structure.push(s)
-    return structure
+
+class Functions():
+    def __init__(self):
+        pass
+
+    def pushSuccessors(self, successors, structure):
+        for s in successors:
+            structure.push(s)
+        return structure
+
+    def isStateVisited(self, visited, state):
+        return state in visited
+
+    def updateSuccessors(self, successors, parent):
+        successorsUpdated = []
+        for s in successors:
+            successorsUpdated.append(
+                (s[0], parent[1] + [s[1]], parent[2] + s[2]))
+
+        return successorsUpdated
+
+    def filterVisitedSuccessors(self, successors, visited):
+        filtered = []
+        for s in successors:
+            if s not in visited:
+                filtered.append(s)
+        return filtered
 
 
-def isStateVisited(visited, state):
-    return state in visited
+class SearchFunctions(Functions):
+    def __init__(self, problem):
+        self.problem = problem
+        super().__init__()
+
+    def pushPQueue(self, successors, pqueue, heuristics=None):
+        for s in successors:
+            if heuristics:
+                distance = heuristics[s[0]]
+                pqueue.push(s, s[2] + distance)
+            else:
+                pqueue.push(s, s[2])
+
+        return pqueue
+
+    def getSuccessorsDistance(self, successors, heuristic):
+        successorsDistance = {}
+        for s in successors:
+            successorsDistance[s[0]] = heuristic(s[0], self.problem)
+
+        return successorsDistance
 
 
-def updateSuccessors(successors, parent):
-    successorsUpdated = []
-    for s in successors:
-        successorsUpdated.append(
-            (s[0], parent[1] + [s[1]], parent[2] + s[2]))
+class CornersFunctions(Functions):
 
-    return successorsUpdated
+    def __init__(self, problem):
+        self.problem = problem
+        super().__init__()
 
+    def pushPQueue(self, successors, pqueue, heuristics=None):
+        for s in successors:
+            if heuristics:
+                distance = heuristics[s[0][0]]
+                pqueue.push(s, s[2] + distance)
+            else:
+                pqueue.push(s, s[2])
 
-def pushPQueue(successors, pqueue, heuristics=None):
-    for s in successors:
-        if heuristics:
-            distance = heuristics[s[0]]
-            pqueue.push(s, s[2] + distance)
-        else:
-            pqueue.push(s, s[2])
+        return pqueue
 
-    return pqueue
-
-
-def getSuccessorsDistance(successors, heuristic, problem):
-    successorsDistance = {}
-
-    for s in successors:
-        successorsDistance[s[0]] = heuristic(s[0], problem)
-
-    return successorsDistance
-
-
-def filterVisitedSuccessors(successors, visited):
-    filtered = []
-    for s in successors:
-        if s not in visited:
-            filtered.append(s)
-    return filtered
+    def getSuccessorsDistance(self, successors, heuristic):
+        successorsDistance = {}
+        for s in successors:
+            position, _ = s[0]
+            successorsDistance[position] = heuristic(s[0], self.problem)
+        return successorsDistance
